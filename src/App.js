9232807f6d044ce701712fdbfa984ce6af2fe425
Su <suce126@126.com>
2019-04-29 00:55:11 +0200

    New functions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Button from "./component/Button";
 import Display from "./component/Display";
+import History from "./component/History";
 
 class App extends Component {
   // constructor(props) {
@@ -11,7 +12,9 @@ class App extends Component {
   state = {
     result: "0",
     result2: "0",
-    operator: null
+    operator: null,
+    resultHistory: [],
+    i:'AC'
   };
 
   buttonPressed = buttonName => {
@@ -19,8 +22,12 @@ class App extends Component {
       this.reset();
     } else if (buttonName === "=") {
       this.calculate();
+    } else if (buttonName === "<") {
+      this.lastResult();
     } else if (buttonName === "±") {
       this.plusminus();
+    } else if (buttonName === "%") {
+      this.percent();
     } else if (buttonName === "+") {
       this.plus();
     } else if (buttonName === "-") {
@@ -29,7 +36,12 @@ class App extends Component {
       this.multiply();
     } else if (buttonName === "/") {
       this.divide();
-    } else if (typeof parseInt(buttonName) === "number") {
+    } else if (buttonName === "√") {
+      this.squareRoot();
+    }
+
+    //else if (buttonName === '.') {this.}
+    else if (typeof parseInt(buttonName) === "number") {
       if (this.state.operator === null) {
         this.setState({
           result:
@@ -57,33 +69,66 @@ class App extends Component {
   };
 
   calculate = () => {
-    if (this.state.operator === "+") {
-      this.setState({
-        result: parseFloat(this.state.result) + parseFloat(this.state.result2),
-        operator: null,
-        result2: "0"
+    return Promise.resolve()
+      .then(() => {
+        if (this.state.operator === "+") {
+          this.setState({
+            result:
+              parseFloat(this.state.result) + parseFloat(this.state.result2),
+            operator: null,
+            result2: "0"
+          });
+        } else if (this.state.operator === "-") {
+          this.setState({
+            result:
+              parseFloat(this.state.result) - parseFloat(this.state.result2),
+            operator: null,
+            result2: "0"
+          });
+        } else if (this.state.operator === "X") {
+          this.setState({
+            result:
+              parseFloat(this.state.result) * parseFloat(this.state.result2),
+            operator: null,
+            result2: "0"
+          });
+        } else if (this.state.operator === "/") {
+          if (this.state.result2 === "0") {
+            this.setState({ result2: "Error" });
+          } else {
+            this.setState({
+              result:
+                parseFloat(this.state.result) / parseFloat(this.state.result2),
+              operator: null,
+              result2: "0"
+            });
+          }
+        }
+      })
+      .then(() => {
+        this.setState({
+          resultHistory: [...this.state.resultHistory, this.state.result]
+        });
       });
-    } else if (this.state.operator === "-") {
-      this.setState({
-        result: parseFloat(this.state.result) - parseFloat(this.state.result2),
-        operator: null,
-        result2: "0",
-      });
-    } else if (this.state.operator === "X") {
-      this.setState({
-        result: parseFloat(this.state.result) * parseFloat(this.state.result2),
-        operator: null,
-        result2: "0",
-      });
-    } else if (this.state.operator === "/") {
-      this.setState({
-        result: parseFloat(this.state.result) / parseFloat(this.state.result2),
-        operator: null,
-        result2: "0",
-      });
-    } else {
-      alert(this.state.result);
-    }
+  };
+
+  lastResult = () => {
+    this.setState({
+      result:
+        this.state.resultHistory.length > 0
+          ? this.state.resultHistory.slice(-1)
+          : "0"
+    });
+  };
+
+  // this.state.result === "0"
+  //             ? buttonName
+  //             : this.state.result + buttonName
+
+  percent = () => {
+    this.setState({
+      result: this.state.result * 0.01
+    });
   };
 
   plus = () => {
@@ -116,6 +161,24 @@ class App extends Component {
     });
   };
 
+  squareRoot = () => {
+    this.setState({
+      result: Math.sqrt(this.state.result).toString()
+    });
+  };
+
+  updateResult = e => {
+    this.setState({
+      result : e
+    })
+  }
+
+  // componentDidUpdate() {
+  //     this.setState({
+  //       i: this.state.result !=='0' ?'C' :'AC'
+  //     })
+  // }
+
   render() {
     let num;
     if (this.state.operator === null) {
@@ -128,7 +191,8 @@ class App extends Component {
         {num}
         {/* <Display ref={this.display} result={this.state.result} /> */}
 
-        <Button buttonPressed={this.buttonPressed} />
+        <Button buttonPressed={this.buttonPressed} i={this.state.i}/>
+        <History resultHistory={this.state.resultHistory} updateResult={this.updateResult}/>
       </div>
     );
   }
--- src/component/Button.js
+++ src/component/Button.js
@@ -4,22 +4,26 @@ import './Button.css'
 export default class Button extends Component {
 
   buttonPressed = e => {
-    console.log(e.target.value);
-    
+    //console.log(e.target.value);
     this.props.buttonPressed(e.target.value);  
   }
   render() {
   return (
     <div className="Main-button">
-        <button type="button" value = 'AC' onClick={this.buttonPressed}>AC</button>
+        <button type="button" value = 'AC' onClick={this.buttonPressed}>{this.props.i}</button>
+        <button type="button" value = '<' onClick={this.buttonPressed}>&larr;</button>
+        <button type="button" value = '>' onClick={this.buttonPressed}></button>
+        <button id='operator' type="button" value = '√' onClick={this.buttonPressed}></button>
+
+        <button type="button" value = '√' onClick={this.buttonPressed}>√</button>
         <button type="button" value = '±' onClick={this.buttonPressed}>±</button>
         <button type="button" value = '%' onClick={this.buttonPressed}>%</button>
-        <button id='operator' type="button" value = '/' onClick={this.buttonPressed}>/</button>
+        <button id='operator' type="button" value = '/' onClick={this.buttonPressed}>÷</button>
 
         <button type="button" value='7' onClick={this.buttonPressed}>7</button>
         <button type="button" value='8' onClick={this.buttonPressed}>8</button>
         <button type="button" value='9' onClick={this.buttonPressed}>9</button>
-        <button id='operator' type="button" value = 'X' onClick={this.buttonPressed} >X</button>
+        <button id='operator' type="button" value = 'X' onClick={this.buttonPressed}>x</button>
       
         <button type="button" value='4' onClick={this.buttonPressed}>4</button>
         <button type="button" value='5'onClick={this.buttonPressed} >5</button>
